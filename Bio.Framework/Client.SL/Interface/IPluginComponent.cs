ffd5935850818a1f122bb6151eb924744c8c148f
Ayrat(home) <gpsdom@gmail.com>
2013-09-19 00:54:21 +0400

    Сохраняюсь перед поездкой в Питер (рассылку еще не доделал)

--- Bio.Framework/Client.SL/Interface/IPluginComponent.cs
+++ Bio.Framework/Client.SL/Interface/IPluginComponent.cs
@@ -1,10 +1,4 @@
 namespace Bio.Framework.Client.SL {
-  using System;
-  using Bio.Helpers.Common.Types;
-  using Bio.Framework.Packets;
-  using System.Windows.Controls;
-  using System.Windows;
-
   public interface IPluginComponent {
     IPlugin ownerPlugin { get; }
   }
