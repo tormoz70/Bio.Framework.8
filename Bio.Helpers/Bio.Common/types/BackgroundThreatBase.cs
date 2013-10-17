6be50665ef42311cb5ebe39bf3228a36a922b137
Ayrat(home) <gpsdom@gmail.com>
2013-10-17 22:20:22 +0400

    немного порешарперил

--- Bio.Helpers/Bio.Common/types/BackgroundThreatBase.cs
+++ Bio.Helpers/Bio.Common/types/BackgroundThreatBase.cs
@@ -29,13 +29,13 @@ namespace Bio.Helpers.Common.Types {
     /// <summary/>
     public ErrorLogWriterDelegate ErrLogWriter { get; set; }
 
-    protected virtual void log_msg(String msg) {
+    protected virtual void logMsg(String msg) {
       lock (this) {
         if (this.MsgLogWriter != null)
           this.MsgLogWriter(msg);
       }
     }
-    protected virtual void log_err(Exception ex) {
+    protected virtual void logErr(Exception ex) {
       lock (this) {
         if (this.ErrLogWriter != null)
           this.ErrLogWriter(ex);
@@ -63,7 +63,7 @@ namespace Bio.Helpers.Common.Types {
     }
 
     private void _trySendMsgToAdmin(String subj, String msg) {
-      this.log_msg("Отправка сообщения администратору ...");
+      this.logMsg("Отправка сообщения администратору ...");
       if ((this.smtpCfg != null) && !String.IsNullOrEmpty(this.adminEmail)) {
         try {
           smtpUtl.Send(
@@ -78,15 +78,15 @@ namespace Bio.Helpers.Common.Types {
             Encoding.Default, 
             Encoding.UTF8
           );
-          this.log_msg(String.Format("Cообщение администратору успешно отправлено на {0}.", this.adminEmail));
+          this.logMsg(String.Format("Cообщение администратору успешно отправлено на {0}.", this.adminEmail));
         } catch (Exception ex) {
-          this.log_msg(String.Format("При отправке сообщения администратору произошла ошибка. Сообщение: {0}\n" +
+          this.logMsg(String.Format("При отправке сообщения администратору произошла ошибка. Сообщение: {0}\n" +
             "Параметры: smtpServer({1}), port({2}), authUser({3}), authPwd({4}), fromMailAddr({5}), encoding({6}), to({7})",
           ex.Message, this.smtpCfg.smtpServer, this.smtpCfg.port, this.smtpCfg.authUser, this.smtpCfg.authPwd,
             this.smtpCfg.fromMailAddr, this.smtpCfg.encoding, this.adminEmail));
         }
       } else {
-        this.log_msg("Отправка сообщения администратору невозможна.");
+        this.logMsg("Отправка сообщения администратору невозможна.");
       }
     }
 
@@ -95,13 +95,13 @@ namespace Bio.Helpers.Common.Types {
       this.rqAbort = false;
       lock(this)
         this.isRunned = true;
-      this.log_msg("Служба запущена.");
-      String v_fatalErr = null;
+      this.logMsg("Служба запущена.");
+      String fatalErr = null;
       try {
-        this.log_msg("Инициализация...");
+        this.logMsg("Инициализация...");
         this.init();
         this.logOutInitInfo();
-        this.log_msg("Инициализация выполнена.");
+        this.logMsg("Инициализация выполнена.");
         this._trySendMsgToAdmin(String.Format("От {0}.", this.serviceName), "Служба запущена. Инициализация выполнена.");
         //var v_hour = DateTime.Now.Hour;
         while (true) {
@@ -111,22 +111,22 @@ namespace Bio.Helpers.Common.Types {
               this.processJob();
             } catch (EBioDBConnectionError ex) {
               var v_msg = String.Format("Ошибка доступа к БД. Служба остановлена на {0} мин.", CI_ON_ERROR_TIMEOUT);
-              this.log_msg(v_msg);
-              this.log_msg("Отладочная информация:");
-              this.log_err(ex);
-              this._trySendMsgToAdmin(String.Format("От {0}.", this.serviceName), v_msg + "\n" + ex.ToString());
+              this.logMsg(v_msg);
+              this.logMsg("Отладочная информация:");
+              this.logErr(ex);
+              this._trySendMsgToAdmin(String.Format("От {0}.", this.serviceName), v_msg + "\n" + ex);
               this._sleepSecs(CI_ON_ERROR_TIMEOUT * 60);
               if (!this.rqAbort)
-                this.log_msg("Служба продолжила работу...");
+                this.logMsg("Служба продолжила работу...");
             } catch (EBioDBAccessError ex) {
               var v_msg = String.Format("Ошибка выполнения запроса к БД. Служба остановлена на {0} мин.", CI_ON_ERROR_TIMEOUT);
-              this.log_msg(v_msg);
-              this.log_msg("Отладочная информация:");
-              this.log_err(ex);
+              this.logMsg(v_msg);
+              this.logMsg("Отладочная информация:");
+              this.logErr(ex);
               this._trySendMsgToAdmin(String.Format("От {0}.", this.serviceName), v_msg + "\n" + ex);
               this._sleepSecs(CI_ON_ERROR_TIMEOUT * 60);
               if (!this.rqAbort) {
-                this.log_msg("Служба продолжила работу...");
+                this.logMsg("Служба продолжила работу...");
                 this._trySendMsgToAdmin(String.Format("От {0}.", this.serviceName), "Служба продолжила работу.");
               }
             } finally {
@@ -139,29 +139,29 @@ namespace Bio.Helpers.Common.Types {
           //this._sleep(20);
           Thread.Sleep(this.sleepMSecsOnEachCicle);
           //if (DateTime.Now.Hour == vHour) {
-          //  this.log_msg("Служба работает нормально...");
+          //  this.logMsg("Служба работает нормально...");
           //  vHour = DateTime.Now.AddHours(1).Hour;
           //}
         }
       } catch(ThreadAbortException) {
         Thread.Sleep(100);
       } catch (Exception ex) {
-        this.log_err(ex);
-        v_fatalErr = ex.ToString();
+        this.logErr(ex);
+        fatalErr = ex.ToString();
       } finally {
         lock (this)
           this.isRunned = false;
-        this.log_msg("Служба остановлена.");
+        this.logMsg("Служба остановлена.");
         this._trySendMsgToAdmin(String.Format("От {0}.", this.serviceName),
-          String.IsNullOrEmpty(v_fatalErr) ? "Служба остановлена. Нет ошибок." : 
-                                              "Служба остановлена. Неизвестная ошибка: " + v_fatalErr);
+          String.IsNullOrEmpty(fatalErr) ? "Служба остановлена. Нет ошибок." : 
+                                              "Служба остановлена. Неизвестная ошибка: " + fatalErr);
       }
     }
 
     /// <summary/>
     public void Start() {
       if (!this.isRunned) {
-        this.log_msg("Запуск службы...");
+        this.logMsg("Запуск службы...");
         this.thread = new Thread(this._execute);
         this.thread.Name = this.serviceName;
         this.thread.Start();
@@ -170,7 +170,7 @@ namespace Bio.Helpers.Common.Types {
     /// <summary/>
     public void Stop() {
       if (this.isRunned) {
-        this.log_msg("Останов службы...");
+        this.logMsg("Останов службы...");
         this.rqAbort = true;
         this.onStop();
       }
